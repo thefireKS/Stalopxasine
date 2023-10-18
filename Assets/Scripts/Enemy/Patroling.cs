18e06d6032500b75e1e35442deaea837db41767b
Sasha <gulixandr@gmail.com>
2023-10-18 14:04:12 +0300

    Enemy slope behavior
    см. breakdown Sasha.md #changes\противник скользит по наклонной поверхности

--- Assets/Scripts/Enemy/Patroling.cs
+++ Assets/Scripts/Enemy/Patroling.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Enemy
 {
@@ -9,15 +10,17 @@ namespace Enemy
 
         [SerializeField] private LayerMask layerMask;
 
-        [SerializeField] protected float rayDistanceToCheckObstacles;
+        [SerializeField] protected float obstaclesCheckRayDistance;
+        [SerializeField] protected float floorCheckRayDistance;
 
         private bool _isHitted;
+        protected float _slopeThreshlold = 2f;  
 
         protected event Action UpdateTimer;
 
         protected Rigidbody2D _rigidbody;
         protected Collider2D _collider;
-
+    
         protected bool _isGoingRight = true;
 
         private void OnEnable()
@@ -47,6 +50,12 @@ namespace Enemy
             _rigidbody.velocity = new Vector2( speedX,_rigidbody.velocity.y);
             SetSpriteRotation();
         }
+        
+        protected void SetSpeed()
+        {
+            var direction = _isGoingRight ? 1 : -1;
+            SetVelocityX(direction * speed);
+        }
 
         private void SetSpriteRotation()
         {
@@ -56,38 +65,45 @@ namespace Enemy
             myTransform.rotation = rotation;
         }
 
-        protected bool CheckObstacles()
+        protected bool CheckObstaclesInFront()
         {
             var bounds = _collider.bounds;
             var direction = _isGoingRight ? 1 : -1;
-            var rayPosition = new Vector3
+            var rayPositionFront = new Vector3
             {
                 x = _isGoingRight ? bounds.max.x : bounds.min.x,
-                y = bounds.min.y //down position
+                y = bounds.center.y //right position
             };
-            Debug.DrawRay(rayPosition, Vector3.down * rayDistanceToCheckObstacles, Color.red);
-            RaycastHit2D hitDown = Physics2D.Raycast(rayPosition, Vector2.down, rayDistanceToCheckObstacles, layerMask);
-            bool isAnythingUnder = hitDown.transform != null;
+            Debug.DrawRay(rayPositionFront, Vector3.right * (direction * obstaclesCheckRayDistance), Color.green);
+            RaycastHit2D hitFront = Physics2D.Raycast(rayPositionFront, Vector2.right * direction, obstaclesCheckRayDistance, layerMask);
 
-            rayPosition = _isGoingRight ? bounds.max : bounds.min;
-            rayPosition.y = bounds.center.y; //right position
-            Debug.DrawRay(rayPosition, Vector3.right * (direction * rayDistanceToCheckObstacles), Color.green);
-            RaycastHit2D hitRight = Physics2D.Raycast(rayPosition, Vector2.right * direction, rayDistanceToCheckObstacles, layerMask);
-            bool isAnythingForward = hitRight.transform != null;
-            return !isAnythingUnder || isAnythingForward;
+            return hitFront.transform != null;
         }
-
+        
         protected bool CheckUnder()
         {
             var bounds = _collider.bounds;
             var rayPosition = new Vector3
             {
-                x = bounds.center.x,
+                x = _isGoingRight ? bounds.max.x : bounds.min.x,
                 y = bounds.min.y //down position
             };
-            Debug.DrawRay(rayPosition, Vector3.down * 0.05f, Color.magenta);
-            RaycastHit2D hitDown = Physics2D.Raycast(rayPosition, Vector2.down, 0.05f, layerMask);
+            Debug.DrawRay(rayPosition, Vector3.down * floorCheckRayDistance, Color.magenta);
+            RaycastHit2D hitDown = Physics2D.Raycast(rayPosition, Vector2.down, floorCheckRayDistance, layerMask);
+            SlopeBehavior(Vector2.Angle(hitDown.normal, Vector2.up));
             return hitDown.transform != null; //something under me = yes
+        }
+        protected void SlopeBehavior(float slopeAngle)
+        {
+            if (slopeAngle > _slopeThreshlold)
+            {
+                if (Mathf.Abs(_rigidbody.velocity.x) < 0.1f)
+                {
+                    _isGoingRight = !_isGoingRight;
+                    SetSpeed();
+                }
+            }
+            
         }
 
         protected override void CollisionBehavior(Collision2D other)
@@ -95,29 +111,27 @@ namespace Enemy
             _isHitted = false;
         }
 
+       
         protected void Patrol()
         {
-            if (CheckObstacles())
+            if (CheckObstaclesInFront() || !CheckUnder())
             {
                 _isGoingRight = !_isGoingRight;
             }
-        
-            var direction = _isGoingRight ? 1 : -1;
-            SetVelocityX(direction * speed);
+            SetSpeed();
         }
 
+        
         protected virtual void Behavior()
         {
             Patrol();
         }
-
         
-
         protected void Update()
         {
             UpdateTimer?.Invoke();
             
-            if (!CheckUnder()) return;
+            if (!CheckUnder()&& Mathf.Abs(_rigidbody.velocity.y)>0.2f) return;//if flying
             if(_isHitted) return;
             
             Behavior();
--- Assets/Scripts/Enemy/Pursuing.cs
+++ Assets/Scripts/Enemy/Pursuing.cs
@@ -26,7 +26,7 @@ namespace Enemy
                     _isGoingRight = true;
                 }
 
-                if (CheckObstacles())
+                if (CheckObstaclesInFront())
                 {
                     _target = null;
                 }
@@ -43,8 +43,8 @@ namespace Enemy
             var rayPosition = bounds.center;
             var direction = _isGoingRight ? 1 : -1;
             RaycastHit2D hit = Physics2D.Raycast(rayPosition, Vector2.right * direction,
-                rayDistanceToCheckObstacles + (bounds.max.x - bounds.center.x), targetMask);
-            Debug.DrawRay(rayPosition, Vector3.right * (direction * rayDistanceToCheckObstacles), Color.yellow);
+                obstaclesCheckRayDistance + (bounds.max.x - bounds.center.x), targetMask);
+            Debug.DrawRay(rayPosition, Vector3.right * (direction * obstaclesCheckRayDistance), Color.yellow);
         
             if (!hit) return;
         
